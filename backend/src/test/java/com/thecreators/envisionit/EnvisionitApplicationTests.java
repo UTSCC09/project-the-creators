4912381296f55113660031963a03de209c5e88c0
Tengokuken <Johnsonz8642@gmail.com>
2022-03-10 03:15:06 -0500

    starter files for backend, gitignore

---
+++ backend/src/test/java/com/thecreators/envisionit/EnvisionitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecreators.envisionit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvisionitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
