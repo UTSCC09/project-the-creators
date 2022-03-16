77ae687948e49a23c0a8ff56d65381fd823165e5
Tengokuken <Johnsonz8642@gmail.com>
2022-03-16 04:32:19 -0400

    base

--- backend/src/main/java/com/thecreators/envisionit/EnvisionitController.java
+++ backend/src/main/java/com/thecreators/envisionit/EnvisionitController.java
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class EnvisionitController {
 
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+
 
 }
