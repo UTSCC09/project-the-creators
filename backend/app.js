3f447316b54cb618977e315a69b45d53b7ca74b9
Tengokuken <Johnsonz8642@gmail.com>
2022-03-24 08:11:31 -0400

    signin signup

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,9 @@ const session = require('express-session');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const cookie = require('cookie');
+const dbo = require('./db/conn');
 
 const app = express();
-
 app.use(session({
     secret: 'thisisasecret',
     resave: false,
@@ -20,13 +20,7 @@ const saltedRounds = 10;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('frontend'));
-
-const Datastore = require('nedb');
-
-let images = new Datastore({ filename: 'db/images.db', autoload: true, timestampData : true});
-let comments = new Datastore({ filename: 'db/comments.db', autoload: true, timestampData: true });
-let users = new Datastore({ filename: 'db/users.db', autoload: true, timestampData: true});
+app.use(express.static('../frontend'));
 
 app.use(function (req, res, next){
     req.username = (req.session.username) ? req.session.username : '';
@@ -34,8 +28,16 @@ app.use(function (req, res, next){
     next();
 });
 
+
 const multer = require('multer');
 const upload = multer({dest: path.join(__dirname, 'uploads/')});
+
+let User = function (username, hashedPassword, email) {
+    this.username = username;
+    this.email = email;
+    this.hashedPassword = hashedPassword;
+    this.creationDate = new Date();
+}
 
 let Image = function (req) {
     const date = new Date();
@@ -65,19 +67,22 @@ function isLast(page, total) {
 
 // curl -H "Content-Type: application/json" -X POST -d '{"username":"alice","password":"alice"}' -c cookie.txt localhost:3000/signup/
 app.post('/signup/', function (req, res, next) {
-    var username = req.body.username;
-    var password = req.body.password;
+    const dbConnect = dbo.getDb();
+    const username = req.body.username;
+    const password = req.body.password;
     bcrypt.hash(password, saltedRounds, function(err, hash) {
         if (err) return res.status(500).end(err);
-        users.findOne({_id: username}, function(err, user){
-            if (err) return res.status(500).end(err);
+        dbConnect.collection('users').findOne({username: username}, function (err, user) {
+            if (err) return res.status(500).end(err, user);
             if (user) return res.status(409).end("username " + username + " already exists");
-            users.update({_id: username},{_id: username, hash}, {upsert: true}, function(err){
+            const userDoc = new User(username, hash, req.body.email);
+            dbConnect.collection('users').insertOne(userDoc, function (err, result) {
                 if (err) return res.status(500).end(err);
+                console.log(`Added a new match with id ${result.insertedId}`);
                 // initialize cookie
                 res.setHeader('Set-Cookie', cookie.serialize('username', username, {
-                      path : '/', 
-                      maxAge: 60 * 60 * 24 * 7
+                        path : '/', 
+                        maxAge: 60 * 60 * 24 * 7
                 }));
                 // create session
                 req.session.username = username;
@@ -87,22 +92,25 @@ app.post('/signup/', function (req, res, next) {
                     maxAge: 60 * 60 * 24 * 7
                 }));
                 return res.json(username);
-            });
-        });
+                //res.status(204).send();
+            })
+        })
     });
 });
 
 
 // curl -H "Content-Type: application/json" -X POST -d '{"username":"alice","password":"alice"}' -c cookie.txt localhost:3000/signin/
 app.post('/signin/', function (req, res, next) {
+    const dbConnect = dbo.getDb();
     req.session.username = req.body.username;
     var username = req.session.username;
     var password = req.body.password;
     // retrieve user from the database
-    users.findOne({_id: username}, function(err, user){
+    dbConnect.collection('users').findOne({username: username}, function (err, user) {
+        console.log(user)
         if (err) return res.status(500).end(err);
         if (!user) return res.status(401).end("The username or password is incorrect.");
-        bcrypt.compare(password, user.hash, function(err, result) {
+        bcrypt.compare(password, user.hashedPassword, function(err, result) {
             if (err) return res.status(500).end(err);
             if (!result) return res.status(401).end("The username or password is incorrect.");
             // initialize cookie
@@ -115,133 +123,143 @@ app.post('/signin/', function (req, res, next) {
     });
 });
 
-// curl -b cookie.txt -c cookie.txt localhost:3000/signout/
-app.get('/signout/', function (req, res, next) {
-    res.setHeader('Set-Cookie', cookie.serialize('username', '', {
-          path : '/', 
-          maxAge: 60 * 60 * 24 * 7 // 1 week in number of seconds
-    }));
-    // delete session
-    req.session.destroy(function(err) {
-        if (err) return res.status(500).end(err);
-        res.json(null);
-    });
-});
+//// curl -b cookie.txt -c cookie.txt localhost:3000/signout/
+//app.get('/signout/', function (req, res, next) {
+//    res.setHeader('Set-Cookie', cookie.serialize('username', '', {
+//          path : '/', 
+//          maxAge: 60 * 60 * 24 * 7 // 1 week in number of seconds
+//    }));
+//    // delete session
+//    req.session.destroy(function(err) {
+//        if (err) return res.status(500).end(err);
+//        res.json(null);
+//    });
+//});
 
-/// Create
-app.post('/api/images', isAuthenticated, upload.single('image'), function (req, res, next) {
-    const image = new Image(req);
-    images.insert(image, function (err, item) {
-        if (err) return res.status(500).end(err);
-        return res.json(item);
-    });
-});
+///// Create
+//app.post('/api/images', isAuthenticated, upload.single('image'), function (req, res, next) {
+//    const image = new Image(req);
+//    images.insert(image, function (err, item) {
+//        if (err) return res.status(500).end(err);
+//        return res.json(item);
+//    });
+//});
 
-app.post('/api/comments', isAuthenticated, function (req, res, next) {
-    const comment = new Comment(req);
-    comments.insert(comment, function (err, item) {
-        if (err) return res.status(500).end(err);
-        return res.json(item);
-    });
-});
+//app.post('/api/comments', isAuthenticated, function (req, res, next) {
+//    const comment = new Comment(req);
+//    comments.insert(comment, function (err, item) {
+//        if (err) return res.status(500).end(err);
+//        return res.json(item);
+//    });
+//});
 
-/// Read
-app.get('/api/images/:id', isAuthenticated, function (req, res, next) {
-    // Return empty object if there are no images
-    images.count({author: req.params.id}, function(err, count) {
-        if (err) return res.status(500).end(err);
-        if (count == 0)
-            return res.json(null);
-        images.find({author: req.params.id}).sort({createdAt:-1}).exec(function(err, items) { 
-            if (err) return res.status(500).end(err);
-            res.json(items.reverse());
-        });
-    });
-});
+///// Read
+//app.get('/api/images/:id', isAuthenticated, function (req, res, next) {
+//    // Return empty object if there are no images
+//    images.count({author: req.params.id}, function(err, count) {
+//        if (err) return res.status(500).end(err);
+//        if (count == 0)
+//            return res.json(null);
+//        images.find({author: req.params.id}).sort({createdAt:-1}).exec(function(err, items) { 
+//            if (err) return res.status(500).end(err);
+//            res.json(items.reverse());
+//        });
+//    });
+//});
 
-app.get('/api/images/:id/image', isAuthenticated, function (req, res, next) {
-    images.findOne({'_id': req.params.id}, function(err, image) {
-        if (err) return res.status(500).end(err);
-        if (!image) return res.status(400).end("Image " + req.params.id + " does not exist!");
-        res.setHeader('Content-Type', image.mimetype);
-        res.sendFile(image.path);
-    });
-});
+//app.get('/api/images/:id/image', isAuthenticated, function (req, res, next) {
+//    images.findOne({'_id': req.params.id}, function(err, image) {
+//        if (err) return res.status(500).end(err);
+//        if (!image) return res.status(400).end("Image " + req.params.id + " does not exist!");
+//        res.setHeader('Content-Type', image.mimetype);
+//        res.sendFile(image.path);
+//    });
+//});
 
-app.get('/api/users/', isAuthenticated, function (req, res, next) {
-    users.count({}, function(err, count) {
-        if (err) return res.status(500).end(err);
-        let page = parseInt(req.query.page) * 20;
-        let isLastPage = isLast(page, count);
-        users.find({}).skip(page).limit(10).exec(function(err, items) {
-            if (err) return res.status(500).end(err);
-            if (!items) return res.status(404).end("Comments under image # " + req.params.id + " do not exist.");
-            // Remove the unneeded information
-            const users = items.map(a => a._id);
-            const result = {
-                'users': users,
-                'isLast': isLastPage
-            };
-            return res.json(result);
-        });
-    });
-});
+//app.get('/api/users/', isAuthenticated, function (req, res, next) {
+//    users.count({}, function(err, count) {
+//        if (err) return res.status(500).end(err);
+//        let page = parseInt(req.query.page) * 20;
+//        let isLastPage = isLast(page, count);
+//        users.find({}).skip(page).limit(10).exec(function(err, items) {
+//            if (err) return res.status(500).end(err);
+//            if (!items) return res.status(404).end("Comments under image # " + req.params.id + " do not exist.");
+//            // Remove the unneeded information
+//            const users = items.map(a => a._id);
+//            const result = {
+//                'users': users,
+//                'isLast': isLastPage
+//            };
+//            return res.json(result);
+//        });
+//    });
+//});
 
-app.get('/api/comments/:id', isAuthenticated, function (req, res, next) {
-    comments.count({'imageId': req.params.id}, function(err, count) {
-        if (err) return res.status(500).end(err);
-        let page = parseInt(req.query.page) * 10;
-        let isLastPage = isLast(page, count);
-        comments.find({'imageId': req.params.id}).sort({createdAt: -1}).skip(page).limit(10).exec(function(err, items) {
-            if (err) return res.status(500).end(err);
-            if (!items) return res.status(404).end("Comments under image # " + req.params.id + " do not exist.");
-            const result = {
-                'comments': items,
-                'isLast': isLastPage
-            };
-            return res.json(result);
-        });
-    });
-});
+//app.get('/api/comments/:id', isAuthenticated, function (req, res, next) {
+//    comments.count({'imageId': req.params.id}, function(err, count) {
+//        if (err) return res.status(500).end(err);
+//        let page = parseInt(req.query.page) * 10;
+//        let isLastPage = isLast(page, count);
+//        comments.find({'imageId': req.params.id}).sort({createdAt: -1}).skip(page).limit(10).exec(function(err, items) {
+//            if (err) return res.status(500).end(err);
+//            if (!items) return res.status(404).end("Comments under image # " + req.params.id + " do not exist.");
+//            const result = {
+//                'comments': items,
+//                'isLast': isLastPage
+//            };
+//            return res.json(result);
+//        });
+//    });
+//});
 
-/// Delete
-app.delete('/api/images/:id', isAuthenticated, function (req, res, next) {
-    images.findOne({_id: req.params.id}, function(err, image) {
-        if (err) return res.status(500).end(err);
-        if (!image) return res.status(404).end("Image with id # " + req.params.id + " does not exist.");
+///// Delete
+//app.delete('/api/images/:id', isAuthenticated, function (req, res, next) {
+//    images.findOne({_id: req.params.id}, function(err, image) {
+//        if (err) return res.status(500).end(err);
+//        if (!image) return res.status(404).end("Image with id # " + req.params.id + " does not exist.");
         
-        images.remove({_id: image._id}, {multi: false}, function(err) {
-            if (err) return res.status(500).end(err);
-            // Delete all of the comments associated with the image as well
-            comments.remove({'imageId': image._id}, {multi: true}, function(err) {
-                if (err) return res.status(500).end(err);
-                // Remove from fs
-                file.unlink(image.path, function (err) {
-                    if (err) return res.status(500).end(err);
-                    res.json(image);
-                });
-            });
-        });
-    });
-});
+//        images.remove({_id: image._id}, {multi: false}, function(err) {
+//            if (err) return res.status(500).end(err);
+//            // Delete all of the comments associated with the image as well
+//            comments.remove({'imageId': image._id}, {multi: true}, function(err) {
+//                if (err) return res.status(500).end(err);
+//                // Remove from fs
+//                file.unlink(image.path, function (err) {
+//                    if (err) return res.status(500).end(err);
+//                    res.json(image);
+//                });
+//            });
+//        });
+//    });
+//});
 
-app.delete('/api/comments/:id', isAuthenticated, function (req, res, next) {
-    comments.findOne({_id: req.params.id}, function(err, comment) {
-        if (err) return res.status(500).end(err);
-        if (!comment) return res.status(404).end("Comment with id # " + req.params.id + " does not exist.");
-        comments.remove({_id: req.params.id}, {multi: false}, function(err) {
-            if (err) return res.status(500).end(err);
-            res.json(comment);
-        });
-    });
-});
+//app.delete('/api/comments/:id', isAuthenticated, function (req, res, next) {
+//    comments.findOne({_id: req.params.id}, function(err, comment) {
+//        if (err) return res.status(500).end(err);
+//        if (!comment) return res.status(404).end("Comment with id # " + req.params.id + " does not exist.");
+//        comments.remove({_id: req.params.id}, {multi: false}, function(err) {
+//            if (err) return res.status(500).end(err);
+//            res.json(comment);
+//        });
+//    });
+//});
 
 const http = require('http');
 const { fstat } = require('fs');
 const { isatty } = require('tty');
 const PORT = 3001;
 
-http.createServer(app).listen(PORT, function (err) {
-    if (err) console.log(err);
-    else console.log("HTTP server on http://localhost:%s", PORT);
+
+dbo.connectToServer(function (err) {
+    if (err) {
+        console.error(err);
+        process.exit();
+    }
+
+    // start the Express server
+    http.createServer(app).listen(PORT, function (err) {
+        if (err) console.log(err);
+        else console.log("HTTP server on http://localhost:%s", PORT);
+    });
 });
+
