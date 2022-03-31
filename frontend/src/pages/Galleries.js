3fccd200f79547d8eee758086bd58a4b6a0a7e0b
Mengqi-Zhao <hart.zhao@mail.utoronto.ca>
2022-03-31 17:45:41 -0400

    Frontend logic for retrieving user data.

--- frontend/src/pages/Galleries.js
+++ frontend/src/pages/Galleries.js
@@ -4,18 +4,36 @@ import { Tab, Tabs } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import Local from "./Local";
 import Shared from "./Shared";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Galleries = () => {
+  const [status, setStatus] = useState({ isLoggedIn: false, user: null });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("/auth/user")
+      .then((res) => {
+        setStatus(res.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <Header />
       <Container className="pt-5 pb-3">
         <Tabs defaultActiveKey="shared" id="accountPG" className="">
           <Tab eventKey="shared" title="Shared">
-            <Shared />
+            <Shared data={status.user} />
           </Tab>
-          <Tab eventKey="local" title="Local"></Tab>
-            <Local />
+          <Tab eventKey="local" title="Local">
+            <Local data={status.user} />
+          </Tab>
         </Tabs>
       </Container>
     </>
