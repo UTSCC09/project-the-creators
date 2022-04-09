406f67ee64f5f6b0f1c2b2468f79f0beff50ab56
Tengokuken <Johnsonz8642@gmail.com>
2022-04-09 17:58:45 -0400

    prod urls

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,4 +1,7 @@
-//export const baseUrl = "https://envisionit.me/api/v1"
- export const authUrl = "http://localhost:3001/auth"
- export const apiUrl = "http://localhost:3001/api"
- export const graphqlUrl = "http://localhost:3001/graphql"
+//export const authUrl = "http://localhost:3001/auth"
+//export const apiUrl = "http://localhost:3001/api"
+//export const graphqlUrl = "http://localhost:3001/graphql"
+
+export const authUrl = "https://envisionit-app.herokuapp.com/auth"
+export const apiUrl = "https://envisionit-app.herokuapp.com/api"
+export const graphqlUrl = "https://envisionit-app.herokuapp.com/graphql"
